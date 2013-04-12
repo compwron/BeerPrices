612ef1a478c8c88d5d6f86f41b5d5407ff6db10b
compwron <wordymusic@gmail.com>
2013-04-12 15:18:40 -0500

    WIP adding tests

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        BeerList beerList = new BeerList(new BeerPropertyGetter("beer.properties").getBeerProperties()); // getBeerProperties returns ArrayList<BeerProperty>
+        BeerList beerList = new BeerList(new BeerPropertyGetter("beer.properties").getBeerProperties());
         System.out.println(beerList.getBeerMenu());
-
     }
 
 }
---
+++ test/BeerPropertyGetterTEst.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class BeerPropertyGetterTest {
+    @Test
+    public void getsPropertiesFrom(){
+
+    }
+}
